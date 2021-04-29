0ae5e48d46ba84cdb9894ec8ad8feab6d36026b2
Bojan Novković <bojano747@gmail.com>
2021-04-29 23:09:03 +0200

    Cmake refactor

--- lib/crt/crt.c
+++ lib/crt/crt.c
@@ -1,14 +1,18 @@
 #include <arch_crt.h>
 #include <sys/cdefs.h>
 
+#ifdef __MUGO_KERNEL__
+#include<kern/util.h>
+
+#endif 
+
 stack_canary_t __stack_chk_guard = ARCH_STACK_CHK_GUARD;
 
 NORETURN void __stack_chk_fail(void)
 {
 
 #ifdef __MUGO_KERNEL__
-#include <kern/util.h>
-
+    
     panic("Kernel stack smashing detected!");
 
 #endif
